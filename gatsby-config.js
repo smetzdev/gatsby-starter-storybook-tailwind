06a78c36cab0c6c872b91fce37b7f020f5d5b69b
smetzdev <metz@raum3.de>
2020-08-28 16:01:44 +0200

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Starter Storybook and Tailwind`,
+    description: `Kick off your next, great Gatsby project with this starter. This barebones starter ships with the main Gatsby configuration files you might need and adds storybook and tailwind.`,
+    author: `@smetzdev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Iam a Headline</h1>
+    <p>Iam some Text... happy hacking with storybook and Tailwind!</p>
+  </Layout>
+)
+
+export default IndexPage
